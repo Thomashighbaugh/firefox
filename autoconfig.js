56a9849ffc3914f45d73748e1610bc87d95e9814
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-05-17 19:16:35 -0700

    first commit

---
+++ autoconfig.js
@@ -0,0 +1,5 @@
+//
+pref("general.config.filename", "autoconfig.cfg");
+pref("general.config.obscure_value", 0);
+pref("general.config.sandbox_enabled", false);
+pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);

91511b9f3a82f7849e5a9e7e4268eb1b47a85056
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-05-19 22:33:42 -0700

    test

--- autoconfig.js
+++ autoconfig.js
@@ -1,5 +1,2 @@
-//
 pref("general.config.filename", "autoconfig.cfg");
 pref("general.config.obscure_value", 0);
-pref("general.config.sandbox_enabled", false);
-pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);

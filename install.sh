350d0324fa170f7737221d42802b709cd9ecc8f1
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-02-19 15:13:40 -0800

    overhaul

--- install.sh
+++ install.sh
@@ -5,31 +5,28 @@ CHROME_DIRECTORY=""
 RELEASE_NAME=""
 
 message() {
-	printf "%s\n" "$*" >&2;
+	printf "%s\n" "$*" >&2
 }
 
 download_bf() {
 
 	message "[>>] Downloading theme..."
 
-	curl -LJ0 https://github.com/manilarome/blurredfox/archive/master.tar.gz | tar -xz -C /tmp/
+	curl -LJ0 https://github.com/Thomashighbaugh/firefox/archive/master.tar.gz | tar -xz -C /tmp/
 
 	# Download success!
-	if [[ $? -eq 0 ]];
-	then
+	if [[ $? -eq 0 ]]; then
 		message "[>>] Copying..."
 
-		FF_THEME="/tmp/blurredfox-master/"
+		FF_THEME="/tmp/firefox-master/"
 		cp -r "${FF_THEME}"* "${CHROME_DIRECTORY}"
 
 		# Backup user.js instead of overwriting it
-		if [ -e "${CHROME_DIRECTORY}/../user.js" ]
-		then
+		if [ -e "${CHROME_DIRECTORY}/../user.js" ]; then
 			message "[>>] Existing user.js detected! Creating backup to user-prefs-backup/..."
 			user_pref_backup_dir="${CHROME_DIRECTORY}/../user-prefs-backup"
 
-			if [[ ! -d "${user_pref_backup_dir}" ]];
-			then
+			if [[ ! -d "${user_pref_backup_dir}" ]]; then
 				message "[>>] user-prefs-backup/ folder does not exist! Creating..."
 				mkdir "${user_pref_backup_dir}"
 			fi
@@ -40,8 +37,7 @@ download_bf() {
 		# Move user.js to the main profile directory
 		mv "${CHROME_DIRECTORY}/user.js" "${CHROME_DIRECTORY}/../"
 
-		if [[ $? -eq 0 ]];
-		then
+		if [[ $? -eq 0 ]]; then
 			rm -rf "/tmp/blurredfox-master"
 		else
 			message " [!!] There was a problem while copying the files. Terminating..."
@@ -62,7 +58,7 @@ download_bf() {
 }
 
 function check_profile() {
-	FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})" 
+	FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})"
 }
 
 function print_help() {
@@ -84,31 +80,24 @@ function print_help() {
 }
 
 # Check args
-if [[ ! -z "${@}" ]] && [[ ! -z "${1}" ]];
-then
+if [[ ! -z "${@}" ]] && [[ ! -z "${1}" ]]; then
 
-	if [[ "${1}" == "dev" ]];
-	then
+	if [[ "${1}" == "dev" ]]; then
 		RELEASE_NAME="Developer Edition"
 		check_profile "dev-edition-default"
-	elif [[ "${1}" == "beta" ]];
-	then
+	elif [[ "${1}" == "beta" ]]; then
 		RELEASE_NAME="Beta"
 		check_profile "default-beta"
-	elif [[ "${1}" == "nightly" ]];
-	then
+	elif [[ "${1}" == "nightly" ]]; then
 		RELEASE_NAME="Nightly"
 		check_profile "default-nightly"
-	elif [[ "${1}" == "stable" ]];
-	then
+	elif [[ "${1}" == "stable" ]]; then
 		RELEASE_NAME="Stable"
 		check_profile "default-release"
-	elif [[ "${1}" == "esr" ]];
-	then
+	elif [[ "${1}" == "esr" ]]; then
 		RELEASE_NAME="ESR"
 		check_profile "default-esr"
-	elif [[ "${1}" == "help" ]];
-	then
+	elif [[ "${1}" == "help" ]]; then
 		print_help
 		exit
 	else
@@ -122,25 +111,21 @@ else
 	check_profile "default-release"
 fi
 
-if [[ -n "$FF_USER_DIRECTORY" ]];
-then
+if [[ -n "$FF_USER_DIRECTORY" ]]; then
 	message "[>>] Firefox user profile directory located..."
 	CHROME_DIRECTORY="$(find "$FF_USER_DIRECTORY/" -maxdepth 1 -type d -name 'chrome')"
-	if [[ -n "$CHROME_DIRECTORY" ]];
-	then
+	if [[ -n "$CHROME_DIRECTORY" ]]; then
 		# Check if the chrome folder is not empty
-		shopt -s nullglob dotglob 
+		shopt -s nullglob dotglob
 		content="${CHROME_DIRECTORY}/"
 
 		# If there's a current theme, make a backup
-		if [ ${#content[@]} -gt 0 ];
-		then
+		if [ ${#content[@]} -gt 0 ]; then
 			message "[>>] Existing chrome folder detected! Creating a backup to chrome-backup/..."
 			backup_dir="${CHROME_DIRECTORY}-backup"
 
 			# Create backup folder
-			if [[ ! -d "${backup_dir}" ]];
-			then
+			if [[ ! -d "${backup_dir}" ]]; then
 				message "[>>] chrome-backup/ folder does not exist! Creating..."
 				mkdir "${backup_dir}"
 			fi
@@ -155,19 +140,18 @@ then
 		mkdir "${FF_USER_DIRECTORY}/chrome"
 
 		# Check if backup folder exist
-		if [[ $? -eq 0 ]];
-		then
+		if [[ $? -eq 0 ]]; then
 			CHROME_DIRECTORY="${FF_USER_DIRECTORY}/chrome"
-			
+
 			# Download theme
 			download_bf
 		else
 			message "[!!] There was a problem while creating the directory. Terminating..."
-			exit 1;
+			exit 1
 		fi
 	fi
 
 else
 	message "[!!] No Firefox ${RELEASE_NAME} user profile detected! Make sure to run Firefox ${RELEASE_NAME} atleast once! Terminating..."
-	exit 1;
+	exit 1
 fi

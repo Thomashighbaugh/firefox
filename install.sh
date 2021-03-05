6a035aecc5b7814ba637a00251846638a65571e2
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-03-04 17:51:40 -0800

    changes to install script

--- install.sh
+++ install.sh
@@ -5,147 +5,158 @@ CHROME_DIRECTORY=""
 RELEASE_NAME=""
 
 message() {
-	printf "%s\n" "$*" >&2
+    printf "%s\n" "$*" >&2
 }
 
 download_ff() {
 
-	message "[>>] Downloading theme..."
+    message "[>>] Begin: Downloading Theme"
 
-	curl -LJ0 https://github.com/Thomashighbaugh/firefox/archive/master.tar.gz | tar -xz -C /tmp
+    curl -LJ0 https://github.com/Thomashighbaugh/firefox/archive/master.tar.gz | tar -xz -C /tmp
 
-	# Download success!
-	if [[ $? -eq 0 ]]; then
-		message "[>>] Copying..."
+    echo"[>>] Download success!"
+    echo "[>>]"
+    echo "[>>]"
+    echo "[>>]"
+    echo "[>>]"
+    echo "[>>] Begin: Installation Phase"
 
-		FF_THEME="/tmp/firefox-master/"
-		cp -r "${FF_THEME}"* "${CHROME_DIRECTORY}"
+    if [[ $? -eq 0 ]]; then
+        message "[>>] Copying..."
 
-		# Backup user.js instead of overwriting it
-		if [ -e "${CHROME_DIRECTORY}/../user.js" ]; then
-			message "[>>] Existing user.js detected! Creating backup to user-prefs-backup/..."
-			user_pref_backup_dir="${CHROME_DIRECTORY}/../user-prefs-backup"
+        FF_THEME="/tmp/firefox-master/"
+        cp -r "${FF_THEME}"* "${CHROME_DIRECTORY}"
 
-			if [[ ! -d "${user_pref_backup_dir}" ]]; then
-				message "[>>] user-prefs-backup/ folder does not exist! Creating..."
-				mkdir "${user_pref_backup_dir}"
-			fi
+        echo "[>>] Backup user.js instead of overwriting it"
 
-			mv --backup=t "${CHROME_DIRECTORY}/../user.js" "${user_pref_backup_dir}"
-		fi
+        if [ -e "${CHROME_DIRECTORY}/../user.js" ]; then
+        
+            message "[>>] Prior Configuration Detected, Creating Backup..."
+            
+            user_pref_backup_dir="${CHROME_DIRECTORY}/../user-prefs-backup"
 
-		# Move user.js to the main profile directory
-		mv "${CHROME_DIRECTORY}/user.js" "${CHROME_DIRECTORY}/../"
+        if [[ ! -d "${user_pref_backup_dir}" ]]; then
+          
+            message "[>>] No Prior Installation Detected, Proceeding As Usual..."
+            
+            mkdir "${user_pref_backup_dir}"
+            fi
 
-		if [[ $? -eq 0 ]]; then
-			rm -rf "/tmp/firefox-master"
-		else
-			message " [!!] There was a problem while copying the files. Terminating..."
-			exit
-		fi
-	else
-		# Download failed
-		message " [!!] There was a problem while downloading the theme. Terminating..."
-		exit
-	fi
-	echo ""
-	message "firefox successfully installed! Enjoy!"
+            mv --backup=t "${CHROME_DIRECTORY}/../user.js" "${user_pref_backup_dir}"
+        fi
+
+        # Move user.js to the main profile directory
+        mv "${CHROME_DIRECTORY}/user.js" "${CHROME_DIRECTORY}/../"
+
+        if [[ $? -eq 0 ]]; then
+            rm -rf "/tmp/firefox-master"
+        else
+            message " [!!] There was a problem while copying the files. Terminating..."
+            exit
+        fi
+    else
+        # Download failed
+        message " [!!] There was a problem while downloading the theme. Terminating..."
+        exit
+    fi
+    echo ""
+    message "[!!] Firefox customization has been successfully installed, please restart your browser to view your new interface."
 }
 
 function check_profile() {
-	FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})"
+    FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})"
 }
 
 function print_help() {
-	echo "Usage:"
-	echo ""
-	echo "help	- Show this message"
-	echo "stable	- Firefox Stable Build"
-	echo "dev 	- Firefox Developer Edition"
-	echo "beta 	- Firefox Beta"
-	echo "nightly - Firefox Nightly"
-	echo "esr 	- Firefox Extended Support Release"
-	echo ""
-	echo "Example:"
+    echo "Usage: run this script followed by the variant of firefox you are installing to"
+    echo "/path/to/this/script/install.sh "
+    echo "[++] help  - Print this help"
+    echo "[++] stable    - Firefox Stable Build"
+    echo "[++] dev   - Firefox Developer Edition"
+    echo "[++] beta  - Firefox Beta"
+    echo "[++] nightly - Firefox Nightly"
+    echo "[++] esr   - Firefox Extended Support Release"
+    echo "[++]"
+    echo "[!!] Example:"
 
-	echo "$ curl -fsSL https://raw.githubusercontent.com/Thomashighbaugh/firefox/script/install.sh | bash -s -- stable"
-	echo ""
-	echo "Defaults to 'stable' if empty."
+    echo "[$$] $ curl -fsSL https://raw.githubusercontent.com/Thomashighbaugh/firefox/script/install.sh | bash -s -- stable"
+    echo ""
+    echo "[!!] The script will default to 'stable' if no option is provided to it, for sake of simplicity and the most common use case."
 }
 
 # Check args
 if [[ ! -z "${@}" ]] && [[ ! -z "${1}" ]]; then
 
-	if [[ "${1}" == "dev" ]]; then
-		RELEASE_NAME="Developer Edition"
-		check_profile "dev-edition-default"
-	elif [[ "${1}" == "beta" ]]; then
-		RELEASE_NAME="Beta"
-		check_profile "default-beta"
-	elif [[ "${1}" == "nightly" ]]; then
-		RELEASE_NAME="Nightly"
-		check_profile "default-nightly"
-	elif [[ "${1}" == "stable" ]]; then
-		RELEASE_NAME="Stable"
-		check_profile "default-release"
-	elif [[ "${1}" == "esr" ]]; then
-		RELEASE_NAME="ESR"
-		check_profile "default-esr"
-	elif [[ "${1}" == "help" ]]; then
-		print_help
-		exit
-	else
-		echo -ne "Invalid parameter!\n"
-		print_help
-		exit
-	fi
+    if [[ "${1}" == "dev" ]]; then
+        RELEASE_NAME="Developer Edition"
+        check_profile "dev-edition-default"
+    elif [[ "${1}" == "beta" ]]; then
+        RELEASE_NAME="Beta"
+        check_profile "default-beta"
+    elif [[ "${1}" == "nightly" ]]; then
+        RELEASE_NAME="Nightly"
+        check_profile "default-nightly"
+    elif [[ "${1}" == "stable" ]]; then
+        RELEASE_NAME="Stable"
+        check_profile "default-release"
+    elif [[ "${1}" == "esr" ]]; then
+        RELEASE_NAME="ESR"
+        check_profile "default-esr"
+    elif [[ "${1}" == "help" ]]; then
+        print_help
+        exit
+    else
+        echo -ne "[!!] Invalid parameter provided, please try using one of the valid parameters as specified in the help message.\n"
+        print_help
+        exit
+    fi
 else
-	# check_profile "(dev-edition|default)-(release|beta|nightly|default|esr)"
-	RELEASE_NAME="Stable"
-	check_profile "default-release"
+    # check_profile "(dev-edition|default)-(release|beta|nightly|default|esr)"
+    RELEASE_NAME="Stable"
+    check_profile "default-release"
 fi
 
 if [[ -n "$FF_USER_DIRECTORY" ]]; then
-	message "[>>] Firefox user profile directory located..."
-	CHROME_DIRECTORY="$(find "$FF_USER_DIRECTORY/" -maxdepth 1 -type d -name 'chrome')"
-	if [[ -n "$CHROME_DIRECTORY" ]]; then
-		# Check if the chrome folder is not empty
-		shopt -s nullglob dotglob
-		content="${CHROME_DIRECTORY}/"
+    message "[>>] Firefox user profile directory located..."
+    CHROME_DIRECTORY="$(find "$FF_USER_DIRECTORY/" -maxdepth 1 -type d -name 'chrome')"
+    if [[ -n "$CHROME_DIRECTORY" ]]; then
+        # Check if the chrome folder is not empty
+        shopt -s nullglob dotglob
+        content="${CHROME_DIRECTORY}/"
 
-		# If there's a current theme, make a backup
-		if [ ${#content[@]} -gt 0 ]; then
-			message "[>>] Existing chrome folder detected! Creating a backup to chrome-backup/..."
-			backup_dir="${CHROME_DIRECTORY}-backup"
+        # If there's a current theme, make a backup
+        if [ ${#content[@]} -gt 0 ]; then
+            message "[>>] Chrome Directory Exists, Backing Up..."
+            backup_dir="${CHROME_DIRECTORY}-backup"
 
-			# Create backup folder
-			if [[ ! -d "${backup_dir}" ]]; then
-				message "[>>] chrome-backup/ folder does not exist! Creating..."
-				mkdir "${backup_dir}"
-			fi
+            # Create backup folder
+            if [[ ! -d "${backup_dir}" ]]; then
+                message "[>>] Chrome Backup Directory Not Detected, Creating One..."
+                mkdir "${backup_dir}"
+            fi
 
-			mv --backup=t "${CHROME_DIRECTORY}" "${backup_dir}"
-			mkdir "${CHROME_DIRECTORY}"
-		fi
-		# Download theme
-		download_ff
-	else
-		message "[>>] Chrome folder does not exist! Creating one..."
-		mkdir "${FF_USER_DIRECTORY}/chrome"
+            mv --backup=t "${CHROME_DIRECTORY}" "${backup_dir}"
+            mkdir "${CHROME_DIRECTORY}"
+        fi
+        # Download theme
+        download_ff
+    else
+        message "[>>] No Chrome Directory Exists, Creating One..."
+        mkdir "${FF_USER_DIRECTORY}/chrome"
 
-		# Check if backup folder exist
-		if [[ $? -eq 0 ]]; then
-			CHROME_DIRECTORY="${FF_USER_DIRECTORY}/chrome"
+        # Check if backup folder exist
+        if [[ $? -eq 0 ]]; then
+            CHROME_DIRECTORY="${FF_USER_DIRECTORY}/chrome"
 
-			# Download theme
-			download_ff
-		else
-			message "[!!] There was a problem while creating the directory. Terminating..."
-			exit 1
-		fi
-	fi
+            # Download theme
+            download_ff
+        else
+            message "[!!] An Error Has Prevented the Directory From Being Created \n Terminating..."
+            exit 1
+        fi
+    fi
 
 else
-	message "[!!] No Firefox ${RELEASE_NAME} user profile detected! Make sure to run Firefox ${RELEASE_NAME} atleast once! Terminating..."
-	exit 1
+    message "[!!] No Firefox ${RELEASE_NAME} user profile detected! Make sure to run Firefox ${RELEASE_NAME} atleast once! \n Terminating..."
+    exit 1
 fi

a10ec80974d71e6eaffb049c25bd7058445ae120
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-10-18 13:36:27 -0700

    userchrome.js

--- install.sh
+++ install.sh
@@ -26,10 +26,10 @@ download_ff() {
         
         FF_THEME="/tmp/firefox-master/"
         cp -r "${FF_THEME}"* "${CHROME_DIRECTORY}"
-        sudo cp -r "${FF_THEME}"patches/root/defaults /usr/lib/firefox*
-        sudo cp -r  "${FF_THEME}"patches/root/config.js /usr/lib/firefox*
-        cp -vrf "${CHROME_DIRECTORY}"/patches/chrome/* "${CHROME_DIRECTORY}" && rm -rvf "${CHROME_DIRECTORY}"/patches
-        
+        sudo cp -r "${FF_THEME}"patches/root/defaults /usr/lib/firefox
+        sudo cp -r  "${FF_THEME}"patches/root/config.js /usr/lib/firefox
+        cp -vf "${CHROME_DIRECTORY}"/patches/utils "${CHROME_DIRECTORY}" && rm -rvf "${CHROME_DIRECTORY}"/patches
+        mv -vf "${CHROME_DIRECTORY}"/userChromeJS/* "${CHROME_DIRECTORY}" && rm -rvf "${CHROME_DIRECTORY}"/userChromeJS
         
         message "[>>] Backup user.js instead of overwriting it"
         
--- patches/root/defaults/pref/config-prefs.js
+++ patches/root/defaults/pref/config-prefs.js
@@ -1,4 +1,3 @@
 pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
-// Sandbox needs to be disabled in release and Beta versions
 pref("general.config.sandbox_enabled", false);

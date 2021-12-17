ed4c4c0035ac4bba2a2a9ea7eb25c3604d14da31
Thomas Highbaugh <thighbaugh@zoho.com>
2021-12-16 17:07:05 -0800

    ugh

--- patches/root/config.js
+++ patches/root/config.js
@@ -1,15 +1,35 @@
 // skip 1st line
 try {
-  let { classes: Cc, interfaces: Ci, manager: Cm, utils: Cu } = Components;
+  
+  let {
+  classes: Cc,
+  interfaces: Ci,
+  manager: Cm,
+  utils: Cu
+  } = Components;
 
-  let cmanifest = Cc["@mozilla.org/file/directory_service;1"]
-    .getService(Ci.nsIProperties)
-    .get("UChrm", Ci.nsIFile);
-  cmanifest.append("utils");
-  cmanifest.append("chrome.manifest");
-
-  if (cmanifest.exists()) {
+  function traverseToMainProfile(str) {
+    let dir = Cc["@mozilla.org/file/directory_service;1"]
+        .getService(Ci.nsIProperties)
+        .get(str, Ci.nsIFile);
+    if (!dir.exists()) {
+        let toAddChrome = false;
+        while (dir.target.includes("chrome_debugger_profile")) {
+            dir = dir.parent;
+            toAddChrome = true;
+        }
+        if (toAddChrome) dir.append("chrome");
+    }
+    return dir;
+}
+  
+  let cmanifest = traverseToMainProfile('UChrm');
+  cmanifest.append('utils');
+  cmanifest.append('chrome.manifest');
+  
+  if(cmanifest.exists()){
     Cm.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-    Cu.import("chrome://userchromejs/content/boot.jsm");
+    Cu.import('chrome://userchromejs/content/boot.jsm');
   }
-} catch (ex) {}
+
+} catch(ex) {};

c5334f4fef3fe9a75abb68558f89f5efcc643e70
Thomashighbaugh <thighbaugh@zoho.com>
2022-06-13 09:50:50 -0700

    mmore icons

--- dev-clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rvf /tmp/*
-rm -rvf ~/.mozilla/firefox/*.default-default/chrome
-rm -rvf ~/.mozilla/firefox/*.default-default/chrome-backup
-rm -rvf ~/.mozilla/firefox/*.default-default/user.js
-rm -rvf ~/.mozilla/firefox/*.default-default/user-prefs-backup

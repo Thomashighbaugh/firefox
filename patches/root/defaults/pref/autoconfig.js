a72f8b1d6e971e71b54c0790ed80580c0dc6d2a2
Thomashighbaugh <thighbaugh@zoho.com>
2022-08-16 11:35:48 -0700

    rebased

--- patches/root/defaults/pref/autoconfig.js
+++
@@ -1,5 +0,0 @@
-// Any comment. You must start the file with a single-line comment!
-
-pref("general.config.filename", "config.js", locked);
-pref("general.config.obscure_value", 0, locked);
-pref("general.config.sandbox_enabled", false, locked);
---
+++ patches/root/defaults/pref/config-prefs.js
@@ -0,0 +1,5 @@
+// starts with blank line
+pref("general.config.obscure_value", 0, locked );
+pref("general.config.filename", "config.js", locked);
+// Sandbox needs to be disabled in release and Beta versions
+pref("general.config.sandbox_enabled", false, locked);

e3f0055f73d1520ef27a0cc5830f480dfc48d06c
Thomashighbaugh <thighbaugh@zoho.com>
2022-08-17 20:22:47 -0700

    ugh

--- patches/root/defaults/pref/config-prefs.js
+++ patches/root/defaults/pref/config-prefs.js
@@ -1,4 +1,4 @@
-pref("general.config.obscure_value", 0);
-pref("general.config.filename", "config.js");
+pref("general.config.obscure_value", 0, locked);
+pref("general.config.filename", "config.js", locked);
 // Sandbox needs to be disabled in release and Beta versions
-pref("general.config.sandbox_enabled", false);
+pref("general.config.sandbox_enabled", false, locked);

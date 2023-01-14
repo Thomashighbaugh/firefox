dddebf5b3ae5fb97de4625b8eef9613b8353fd53
Thomashighbaugh <thighbaugh@zoho.com>
2023-01-14 16:06:06 +0000

    scss

--- css.js
+++ css.js
@@ -1,12 +1,16 @@
-var fs = require('fs');
-var cssscss = require('css-scss');
+/* ------------- CSS to SCSS Converter ------------- */
+// This will convert userChrome and userContent to SCSS files, which was somewhat helpful when working with the Dark Matter CSS
 
-var src = fs.readFileSync('./userChrome.css', 'utf8');
-var scss = cssscss(src);
+// WARNING: if working with firefox variables, first you will need to remove them from the file as they will be horribly scrambled by this,
+var fs = require("fs");
+var cssScss = require("css-scss");
 
-fs.writeFileSync('./src/higgsBosonChrome.scss', scss);
+// var src = fs.readFileSync('./userChrome.css', 'utf8');
+// var scss = cssScss(src);
 
-var src = fs.readFileSync('./userContent.css', 'utf8');
-var scss = cssscss(src);
+// fs.writeFileSync('./userChrome.scss', scss);
 
-fs.writeFileSync('./src/higgsBosonContent.scss', scss);
+var src = fs.readFileSync("./userContent.css", "utf8");
+var scss = cssScss(src);
+
+fs.writeFileSync("./userContent.scss", scss);

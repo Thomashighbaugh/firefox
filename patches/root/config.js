803f67330c4aa280b2a4e80edc06c61c8f186ee5
Thomashighbaugh <thighbaugh@zoho.com>
2024-07-15 08:49:00 -0700

    rollbacl

--- patches/root/config.js
+++ patches/root/config.js
@@ -1,13 +1,13 @@
 // skip 1st line
 try {
-    
-  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
-  cmanifest.append('utils');
-  cmanifest.append('chrome.manifest');
-  
-  if(cmanifest.exists()){
-    Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-    ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
-  }
+  let { classes: Cc, interfaces: Ci, manager: Cm, utils: Cu } = Components;
 
-} catch(ex) {};
+  let cmanifest = Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("UChrm", Ci.nsIFile);
+  cmanifest.append("utils");
+  cmanifest.append("chrome.manifest");
+
+  if (cmanifest.exists()) {
+    Cm.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
+    Cu.import("chrome://userchromejs/content/boot.jsm");
+  }
+} catch (ex) {}

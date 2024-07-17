6558bc02acb04068fac4d4264b8ed9eeb9055776
Thomashighbaugh <thighbaugh@zoho.com>
2024-07-17 01:53:10 -0700

    .sys.mjs

--- patches/root/config.js
+++ patches/root/config.js
@@ -1,13 +1,15 @@
 // skip 1st line
 try {
-    
-  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
-  cmanifest.append('utils');
-  cmanifest.append('chrome.manifest');
-  
-  if(cmanifest.exists()){
-    Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-    ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
-  }
+  let cmanifest = Cc["@mozilla.org/file/directory_service;1"]
+    .getService(Ci.nsIProperties)
+    .get("UChrm", Ci.nsIFile);
+  cmanifest.append("utils");
+  cmanifest.append("chrome.manifest");
 
-} catch(ex) {};
+  if (cmanifest.exists()) {
+    Components.manager
+      .QueryInterface(Ci.nsIComponentRegistrar)
+      .autoRegister(cmanifest);
+    ChromeUtils.importESModule("chrome://userchromejs/content/boot.sys.mjs");
+  }
+} catch (ex) {}

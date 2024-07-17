997bc11be594ceb129e4a8d00861b1a1ca96e06b
Thomashighbaugh <thighbaugh@zoho.com>
2024-07-17 04:07:47 -0700

    ultima?

--- install.sh
+++ install.sh
@@ -10,63 +10,74 @@
 # Author:     Thomas Leon Highbaugh<admin@thomasleonhighbaugh.me>
 #-----------------------------------------------------------------#
 
+# Define global variables
 FF_USER_DIRECTORY=""
 CHROME_DIRECTORY=""
 RELEASE_NAME=""
+PROGRAM_DIRECTORY=""
+THEME_DIRECTORY="/tmp/firefox"
 
+# Function to print messages
 message() {
 	printf "%s\n" "$*" >&2
 }
 
+# Function to download the theme
 download_ff() {
-
 	message "[>>] Downloading theme..."
-	if [[ ! -d "/tmp/firefox" ]]; then
-	  sudo git clone https://github.com/Thomashighbaugh/firefox /tmp/firefox >/dev/null 2>&1
-    else 
-       sudo rm -rf  /tmp/firefox  >/dev/null 2>&1
-       sudo git clone https://github.com/Thomashighbaugh/firefox /tmp/firefox >/dev/null 2>&1
-    fi
-	#---------------------------------------------------------#
-	# Download success!
+
+	# Clean up previous download if it exists
+	if [[ -d "${THEME_DIRECTORY}" ]]; then
+		sudo rm -rf "${THEME_DIRECTORY}" >/dev/null 2>&1
+	fi
+
+	# Download the theme from GitHub
+	sudo git clone https://github.com/Thomashighbaugh/firefox "${THEME_DIRECTORY}" >/dev/null 2>&1
+
+	# Check if download was successful
 	if [[ $? -eq 0 ]]; then
 		message "[>>] Copying..."
-		FF_THEME="/tmp/firefox/"
-		#---------------------------------------------------------#
-		# Copy the Chrome Files to the User Directory in the Profile Determined by the `check_profile` function
+		FF_THEME="${THEME_DIRECTORY}/"
+
+		# Copy theme files to the user's chrome directory
 		cp -r "${FF_THEME}"* "${CHROME_DIRECTORY}"
-		#---------------------------------------------------------#
-		# Copy the relevant patches to the program itself from the patches subdirectory using sudo permissions
-		sudo cp -rvf /tmp/firefox/patches/root/* $PROGRAM_DIRECTORY
-		#---------------------------------------------------------#
-		# Backup user.js instead of overwriting it
+
+		# Copy patches to the program directory
+		sudo cp -rvf "${THEME_DIRECTORY}/patches/root"/* "${PROGRAM_DIRECTORY}"
+
+		# Backup existing user.js
 		if [ -e "${CHROME_DIRECTORY}/../user.js" ]; then
 			message "[>>] Existing user.js detected! Creating backup to user-prefs-backup/..."
+
 			user_pref_backup_dir="${CHROME_DIRECTORY}/../user-prefs-backup"
-			#---------------------------------------------------------#
+
+			# Create backup directory if it doesn't exist
 			if [[ ! -d "${user_pref_backup_dir}" ]]; then
 				message "[>>] user-prefs-backup/ folder does not exist! Creating..."
 				mkdir "${user_pref_backup_dir}"
 			fi
 
+			# Move user.js to the backup directory
 			mv --backup=t "${CHROME_DIRECTORY}/../user.js" "${user_pref_backup_dir}"
 		fi
-		#---------------------------------------------------------#
-		# Move user.js to the main profile directory
+
+		# Move new user.js to the main profile directory
 		mv "${CHROME_DIRECTORY}/user.js" "${CHROME_DIRECTORY}/../"
 
+		# Clean up temporary theme directory
 		if [[ $? -eq 0 ]]; then
-		sudo rm -rf "/tmp/firefox/"  >/dev/null  2>&1
+			sudo rm -rf "${THEME_DIRECTORY}" >/dev/null 2>&1
 		else
 			message " [!!] There was a problem while copying the files. Terminating..."
 			exit
 		fi
 	else
-		#---------------------------------------------------------#
 		# Download failed
 		message " [!!] There was a problem while downloading the theme. Terminating..."
 		exit
 	fi
+
+	# Print success message
     clear
 	echo "#########################################"
 	echo "##   _______ __                        ##"
@@ -78,21 +89,25 @@ download_ff() {
 	echo "##  |   __ \.-----.-----.-----.-----.  ##"
 	echo "##  |   __ <|  _  |__ --|  _  |     |  ##"
 	echo "##  |______/|_____|_____|_____|__|__|  ##"
-    echo "##                                     ##"
+  echo "##                                     ##"
 	echo "#########################################"
 	echo
     message "#########################################"
-    message "## Please restart your browser.        ##"
-    message "## Enjoy!                              ##"
+    message "##                                     ##"
+    message "## Please restart your browser!        ##"
+    message "##                                     ##"
     message "#########################################"
     echo
     echo
 }
 
+# Function to check the Firefox profile directory
 function check_profile() {
-	FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})"
+  
+  FF_USER_DIRECTORY="$(find "${HOME}/.mozilla/firefox/" -maxdepth 1 -type d -regextype egrep -regex '.*[a-zA-Z0-9]+.'${1})" 
 }
 
+# Function to print help message
 function print_help() {
 
 	echo "------------------------------------------------"
@@ -114,11 +129,13 @@ function print_help() {
 	echo "Defaults to 'stable' if empty."
 	echo "------------------------------------------------"
 }
+
 #-------------------------------------------------------#
 #-----------------Check Arguments-----------------------#
 #-------------------------------------------------------#
 if [[ ! -z "${@}" ]] && [[ ! -z "${1}" ]]; then
 
+	# Check which Firefox version is selected and set the corresponding variables
 	if [[ "${1}" == "dev" ]]; then
 		RELEASE_NAME="Firefox Developer Edition"
 		check_profile "dev-edition-default"
@@ -148,60 +165,66 @@ if [[ ! -z "${@}" ]] && [[ ! -z "${1}" ]]; then
 		PROGRAM_DIRECTORY="/usr/lib/librewolf/"
 	else
 		echo -ne "Invalid parameter!\n"
-		print_help "default-release"
+		print_help
 		exit
 	fi
 else
-	# check_profile "(dev-edition|default)-(release|beta|nightly|default|esr)"
+	# Default to stable Firefox if no argument is provided
 	RELEASE_NAME="Stable"
 	PROGRAM_DIRECTORY="/usr/lib/firefox*"
 	check_profile "default-release"
 fi
+
 #----------------------------------------------------#
+# Check if the Firefox user profile directory is found
 if [[ -n "$FF_USER_DIRECTORY" ]]; then
 	message "[>>] Firefox user profile directory located..."
+
+	# Find the chrome directory within the user profile
 	CHROME_DIRECTORY="$(find "$FF_USER_DIRECTORY/" -maxdepth 1 -type d -name 'chrome')"
+
+	# Check if the chrome directory exists
 	if [[ -n "$CHROME_DIRECTORY" ]]; then
-		# Check if the chrome folder is not empty
+		# Enable nullglob and dotglob to make sure that we handle cases where the chrome directory is empty
 		shopt -s nullglob dotglob
+
+		# Check if the chrome directory has contents
 		content="${CHROME_DIRECTORY}/"
-		#---------------------------------------------------------#
-		# If there's a current theme, make a backup
 		if [ ${#content[@]} -gt 0 ]; then
 			message "[>>] Existing chrome folder detected! Creating a backup to chrome-backup/..."
+
 			backup_dir="${CHROME_DIRECTORY}-backup"
-			#---------------------------------------------------------#
-			# Create backup folder
+
+			# Create backup directory if it doesn't exist
 			if [[ ! -d "${backup_dir}" ]]; then
 				message "[>>] chrome-backup/ folder does not exist! Creating..."
 				mkdir "${backup_dir}"
 			fi
 
+			# Backup the existing chrome directory
 			mv --backup=t "${CHROME_DIRECTORY}" "${backup_dir}"
 			mkdir "${CHROME_DIRECTORY}"
 		fi
-		# Download theme
+
+		# Download the theme
 		download_ff
 	else
 		message "[>>] Chrome folder does not exist! Creating one..."
+
+		# Create the chrome directory
 		mkdir "${FF_USER_DIRECTORY}/chrome"
-		#---------------------------------------------------------#
-		# Check if backup folder exist
+
+		# Check if directory creation was successful
 		if [[ $? -eq 0 ]]; then
 			CHROME_DIRECTORY="${FF_USER_DIRECTORY}/chrome"
-			#-----------------------------------------------------------#
-			#----------------------Download Theme-----------------------#
-			#-----------------------------------------------------------#
 			download_ff
 		else
 			message "[!!] There was a problem while creating the directory. Terminating..."
 			exit 1
 		fi
 	fi
-#-----------------------------------------------#
-#---------Extreme Error Handling Case-----------#
-#-----------------------------------------------#
 else
-	message "[!!] No ${RELEASE_NAME} user profile detected! Make sure to run Firefox ${RELEASE_NAME} atleast once! Terminating..."
+	# No user profile found, exit with error message
+	message "[!!] No ${RELEASE_NAME} user profile detected! Make sure to run Firefox ${RELEASE_NAME} at least once! Terminating..."
 	exit 1
 fi
--- user.js
+++ user.js
@@ -42,3 +42,50 @@ user_pref("privacy.popups.disable_from_plugins", 0);
 // Enable Global Privacy Control functionality thus enriching others being creepy less 
 user_pref("privacy.globalprivacycontrol.enabled", true);
 user_pref("privacy.globalprivacycontrol.functionality.enabled", true);
+
+/* Ultima Settings */ 
+/*tabs*/
+user_pref("ultima.tabs.size.xs", false);
+user_pref("ultima.tabs.size.s", false);
+user_pref("ultima.tabs.size.l", true);
+user_pref("ultima.tabs.autohide", true);
+user_pref("ultima.tabs.closetabsbutton", true);
+user_pref("ultima.tabs.vertical.hide", false);
+user_pref("ultima.tabs.vertical.onlyprivate", false);
+
+/*sidebar*/
+user_pref("ultima.sidebar.autohide", false);
+user_pref("ultima.sidebery.autohide", false);
+
+/*extension theme on-off*/
+user_pref("ultima.theme.extensions", true);
+
+/*url bar*/
+user_pref("ultima.urlbar.suggestions", true);
+user_pref("ultima.urlbar.centered", true);
+user_pref("ultima.urlbar.hidebuttons", false);
+
+/*alternate styles*/
+user_pref("ultima.xstyle.containertabs.i", false);
+user_pref("ultima.xstyle.containertabs.ii", false);
+user_pref("ultima.xstyle.containertabs.iii", true);
+user_pref("ultima.xstyle.squared", false);
+user_pref("ultima.xstyle.lwtheme", false); /* Experimental. To counter addon themes that use unusual color variables. */
+
+/*specific OS overrides (like titlebar buttons)*/
+user_pref("ultima.OS.kde", false);
+user_pref("ultima.OS.gnome", true);
+user_pref("ultima.OS.mac", false);
+user_pref("ultima.OS.kdedecorations", false);
+user_pref("ultima.OS.linux", true);
+user_pref("ultima.OS.gnomedecorations", true); /*Experimental*/
+
+/*other*/
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.prompt-connection", false);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.tabbox.switchByScrolling", true);
+

fce2ab1ce79d374270d38407973dddf302884410
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2024-12-26 20:20:46 -0800

    feat: add sidebar CSS for dynamic width adjustment on hover

--- JS/statusbar.uc.js
+++ JS/statusbar.uc.js
@@ -1,171 +1,193 @@
-"use strict";
 // ==UserScript==
-// @name            Statusbar
-// @author          Thomas Leon Highbaugh
-// @version         0.1
-// @description     Patches the sidebar to allow you to auto-hide it once more!
+// @name            Status Bar
+// @author          xiaoxiaoflood
+// @include         main
+// @startup         UC.statusBar.exec(win);
+// @shutdown        UC.statusBar.destroy();
 // @onlyonce
 // ==/UserScript==
-(function() {
-  if (appversion >= 76 && location != "chrome://browser/content/browser.xhtml")
-    return;
 
-  var compact_buttons = false; // reduced toolbar height and smaller buttons
+const { CustomizableUI, StatusPanel } = window;
 
-  var AddStatusbar = {
-    init: function () {
-      /* blank tab workaround */ xxxxx;
-      try {
-        if (gBrowser.selectedBrowser.getAttribute("blank"))
-          gBrowser.selectedBrowser.removeAttribute("blank");
-      } catch (e) {}
+UC.statusBar = {
+  PREF_ENABLED: 'higgs-boson.statusbar.enabled',
+  PREF_STATUSTEXT: 'higgs-boson.statusbar.appendStatusText',
 
-      try {
-        Services.prefs
-          .getDefaultBranch("browser.statusbar.")
-          .setBoolPref("enabled", true);
-      } catch (e) {}
+  get enabled() {
+    return xPref.get(this.PREF_ENABLED);
+  },
 
-      var statusbar_label = "Statusbar";
-      var compact_buttons_code = "";
+  get textInBar() {
+    return this.enabled && xPref.get(this.PREF_STATUSTEXT);
+  },
 
-      if (compact_buttons)
-        compact_buttons_code =
-          "\
-		#statusbar toolbarbutton .toolbarbutton-icon { \
-		  padding: 0 !important; \
-		  width: 16px !important; \
-		  height: 16px !important; \
-		} \
-		#statusbar .toolbarbutton-badge-stack { \
-		  padding: 0 !important; \
-		  margin: 0 !important; \
-		  width: 16px !important; \
-		  min-width: 16px !important; \
-		  height: 16px !important; \
-		  min-height: 16px !important; \
-		} \
-		#statusbar toolbarbutton .toolbarbutton-badge { \
-		  margin-top: 0px !important; \
-		  font-size:10px !important; \
-		} \
-	  ";
+  init: function () {
+    xPref.set(this.PREF_ENABLED, true, true);
+    xPref.set(this.PREF_STATUSTEXT, true, true);
+    this.enabledListener = xPref.addListener(this.PREF_ENABLED, (isEnabled) => {
+      CustomizableUI.getWidget('status-dummybar').instances.forEach(dummyBar => {
+        dummyBar.node.setAttribute('collapsed', !isEnabled);
+      });
+    });
+    this.textListener = xPref.addListener(this.PREF_STATUSTEXT, (isEnabled) => {
+      if (!UC.statusBar.enabled)
+        return;
 
-      // style sheet
-      Components.classes["@mozilla.org/content/style-sheet-service;1"]
-        .getService(Components.interfaces.nsIStyleSheetService)
-        .loadAndRegisterSheet(
-          Services.io.newURI(
-            "data:text/css;charset=utf-8," +
-              encodeURIComponent(
-                '\
-		  \
-		  #statusbar toolbarpaletteitem[place=toolbar][id^=wrapper-customizableui-special-spring],\
-		  #statusbar toolbarspring {\
-			-moz-box-flex: 1 !important;\
-			min-width: 100% !important;\
-			width: unset !important;\
-			max-width: unset !important;\
-		  }\
-		  #main-window[customizing] #statusbar { \
-			outline: 1px dashed !important; \
-			outline-offset: -2px !important; \
-		  } \
-		  #statusbar { \
-			border-top: 1.5px solid var(--sidebar-border-color,rgba(0,0,0,0.3)) !important; \
-			min-height: 20px !important;\
-			background-color: var(--window-color); \
-			background-image: var(--toolbar-bgimage); \
-			-moz-window-dragging: no-drag !important; \
-		  }\
-		  #main-window:-moz-lwtheme #statusbar { \
-			background: var(--lwt-accent-color) !important; \
-		  }\
-		  #main-window[lwtheme-image="true"]:-moz-lwtheme #statusbar { \
-			background: var(--lwt-header-image) !important; \
-			background-position: 0vw 50vh !important; \
-		  }\
-		  /* autohide add-on bar in fullscreen mode */ \
-		  #main-window[sizemode="fullscreen"]:not([inDOMFullscreen="true"]) #statusbar {\
-			visibility: visible !important; \
-			display: block !important; \
-			min-height: 1px !important; \
-			height: 1px !important; \
-			max-height: 1px !important; \
-		  } \
-		  #main-window[sizemode="fullscreen"]:not([inDOMFullscreen="true"]) #statusbar:hover {\
-			min-height: 24px !important; \
-			height: 24px !important; \
-			max-height: 24px !important; \
-		  } \
-		  ' +
-                compact_buttons_code +
-                "\
-	  ",
-              ),
-            null,
-            null,
-          ),
-          Components.classes[
-            "@mozilla.org/content/style-sheet-service;1"
-          ].getService(Components.interfaces.nsIStyleSheetService).AGENT_SHEET,
-        );
+      _uc.windows((doc, win) => {
+        let StatusPanel = win.StatusPanel;
+        if (isEnabled)
+          win.statusbar.textNode.appendChild(StatusPanel._labelElement);
+        else
+          StatusPanel.panel.appendChild(StatusPanel._labelElement);
+      });
+    });
 
-      // toolbar
-      try {
-        if (document.getElementById("statusbar") == null) {
-          var tb_statusbar = document.createXULElement("toolbar");
-          if (appversion <= 62) tb_statusbar = document.createElement("toolbar");
-          tb_statusbar.setAttribute("id", "statusbar");
-          tb_statusbar.setAttribute("collapsed", "false");
-          tb_statusbar.setAttribute("toolbarname", statusbar_label);
-          tb_statusbar.setAttribute("defaultset", "spring,spring");
-          tb_statusbar.setAttribute("customizable", "true");
-          tb_statusbar.setAttribute("mode", "icons");
-          tb_statusbar.setAttribute("iconsize", "small");
-          tb_statusbar.setAttribute("context", "toolbar-context-menu");
-          tb_statusbar.setAttribute("lockiconsize", "true");
-          tb_statusbar.setAttribute(
-            "class",
-            "toolbar-primary chromeclass-toolbar browser-toolbar customization-target",
-          );
+    this.setStyle();
+    _uc.sss.loadAndRegisterSheet(this.STYLE.url, this.STYLE.type);
 
-          document.getElementById("browser-bottombox").appendChild(tb_statusbar);
+    CustomizableUI.registerArea('status-bar', {});
 
-          CustomizableUI.registerArea("statusbar", { legacy: true });
+    Services.obs.addObserver(this, 'browser-delayed-startup-finished');
+  },
 
-          if (appversion >= 65) {
-            CustomizableUI.registerToolbarNode(tb_statusbar);
-          }
+  exec: function (win) {
+    let document = win.document;
+    let StatusPanel = win.StatusPanel;
 
-          // 'Ctr + /' on Windows/Linux or 'Cmd + /' on macOS to toggle add-on bar
-          var key = document.createXULElement("key");
-          if (appversion < 69) key = document.createElement("key");
-          key.id = "key_toggleStatusBar";
-          key.setAttribute("key", "/");
-          key.setAttribute("modifiers", "accel");
-          key.setAttribute(
-            "oncommand",
-            'var newStatusbar = document.getElementById("statusbar"); setToolbarVisibility(newStatusbar, newStatusbar.collapsed);Services.prefs.getBranch("browser.statusbar.").setBoolPref("enabled",!newStatusbar.collapsed)',
-          );
-          document.getElementById("mainKeyset").appendChild(key);
+    let dummystatusbar = _uc.createElement(document, 'toolbar', {
+      id: 'status-dummybar',
+      toolbarname: 'Status Bar',
+      accesskey: 'S',
+      hidden: 'true'
+    });
+    dummystatusbar.collapsed = !this.enabled;
+    dummystatusbar.setAttribute = function (att, value) {
+      let result = Element.prototype.setAttribute.apply(this, arguments);
 
-          try {
-            setToolbarVisibility(
-              document.getElementById("statusbar"),
-              Services.prefs
-                .getBranch("browser.statusbar.")
-                .getBoolPref("enabled"),
-            );
-          } catch (e) {}
+      if (att == 'collapsed') {
+        let StatusPanel = win.StatusPanel;
+        if (value === true) {
+          xPref.set(UC.statusBar.PREF_ENABLED, false);
+          win.statusbar.node.setAttribute('collapsed', true);
+          StatusPanel.panel.appendChild(StatusPanel._labelElement);
+          win.statusbar.node.parentNode.collapsed = true;;
+        } else {
+          xPref.set(UC.statusBar.PREF_ENABLED, true);
+          win.statusbar.node.setAttribute('collapsed', false);
+          if (UC.statusBar.textInBar)
+            win.statusbar.textNode.appendChild(StatusPanel._labelElement);
+          win.statusbar.node.parentNode.collapsed = false;
         }
-      } catch (e) {}
-    },
-  };
+      }
 
-  AddStatusbar.init();
+      return result;
+    };
+    win.gNavToolbox.appendChild(dummystatusbar);
 
-  setTimeout(function(){
-    AddStatusbar.init();
-  },2000);
-})();
+    win.statusbar.node = _uc.createElement(document, 'toolbar', {
+      id: 'status-bar',
+      customizable: 'true',
+      context: 'toolbar-context-menu',
+      mode: 'icons'
+    });
+
+    win.statusbar.textNode = _uc.createElement(document, 'toolbaritem', {
+      id: 'status-text',
+      flex: '1',
+      width: '100'
+    });
+    if (this.textInBar)
+      win.statusbar.textNode.appendChild(StatusPanel._labelElement);
+    win.statusbar.node.appendChild(win.statusbar.textNode);
+
+    win.eval('Object.defineProperty(StatusPanel, "_label", {' + Object.getOwnPropertyDescriptor(StatusPanel, '_label').set.toString().replace(/^set _label/, 'set').replace(/((\s+)this\.panel\.setAttribute\("inactive", "true"\);)/, '$2this._labelElement.value = val;$1') + ', enumerable: true, configurable: true});');
+
+    let bottomBox = document.createElement('vbox');
+    bottomBox.id = 'browser-bottombox';
+    bottomBox.append(win.statusbar.node);
+
+    if (!this.enabled)
+      bottomBox.collapsed = true;
+
+    document.getElementById('fullscreen-and-pointerlock-wrapper').insertAdjacentElement('afterend', bottomBox);
+
+    win.addEventListener('fullscreen', this.fsEvent);
+
+    if (document.readyState === 'complete')
+      this.observe(win);
+  },
+
+  fsEvent: function (ev) {
+    const { StatusPanel, fullScreen, statusbar } = ev.target;
+    if (fullScreen)
+      StatusPanel.panel.appendChild(StatusPanel._labelElement);
+    else
+      statusbar.textNode.appendChild(StatusPanel._labelElement);
+  },
+
+  observe: function (win) {
+    CustomizableUI.registerToolbarNode(win.statusbar.node);
+  },
+
+  orig: Object.getOwnPropertyDescriptor(StatusPanel, '_label').set.toString(),
+
+  setStyle: function () {
+    this.STYLE = {
+      url: Services.io.newURI('data:text/css;charset=UTF-8,' + encodeURIComponent(`
+        @-moz-document url('${_uc.BROWSERCHROME}') {
+          #status-bar {
+            color: initial !important;
+            background-color: var(--toolbar-non-lwt-bgcolor) !important;
+          }
+          #status-text > #statuspanel-label {
+            border-top: 0 !important;
+            background-color: unset !important;
+            color: #444;
+          }
+          #status-bar > #status-text {
+            display: flex !important;
+            justify-content: center !important;
+            align-content: center !important;
+            flex-direction: column !important;
+            -moz-window-dragging: drag;
+          }
+          toolbarpaletteitem #status-text:before {
+            content: "Status text";
+            color: red;
+            border: 1px #aaa solid;
+            border-radius: 3px;
+            font-weight: bold;
+          }
+          #browser-bottombox:not([collapsed]) {
+            border-top: 1px solid #BCBEBF !important;
+          }
+          :root[inFullscreen]:not([macOSNativeFullscreen]) #browser-bottombox {
+            visibility: collapse !important;
+          }
+        }
+      `)),
+      type: _uc.sss.USER_SHEET
+    }
+  },
+
+  destroy: function () {
+    const { CustomizableUI } = Services.wm.getMostRecentBrowserWindow();
+
+    xPref.removeListener(this.enabledListener);
+    xPref.removeListener(this.textListener);
+    CustomizableUI.unregisterArea('status-bar');
+    _uc.sss.unregisterSheet(this.STYLE.url, this.STYLE.type);
+    _uc.windows((doc, win) => {
+      const { eval, statusbar, StatusPanel } = win;
+      eval('Object.defineProperty(StatusPanel, "_label", {' + this.orig.replace(/^set _label/, 'set') + ', enumerable: true, configurable: true});');
+      StatusPanel.panel.appendChild(StatusPanel._labelElement);
+      doc.getElementById('status-dummybar').remove();
+      statusbar.node.remove();
+      win.removeEventListener('fullscreen', this.fsEvent);
+    });
+    Services.obs.removeObserver(this, 'browser-delayed-startup-finished');
+    delete UC.statusBar;
+  }
+}
+
+UC.statusBar.init();

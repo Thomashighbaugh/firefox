8d404d34804f18b76ee6feb46e99f4bd34cee0de
Thomas Leon Highbaugh <me@thomasleonhighbaugh.me>
2025-09-06 11:29:09 -0700

    2025-09-06 11:29:09

--- JS/extendedStatusbar.uc.js
+++ JS/extendedStatusbar.uc.js
@@ -109,17 +109,17 @@ var AddAddonbar = {
 		  }
 		  #addonbar {
             border-radius: 6px 6px 0 0;
-			background-color: #1D1B22 !important;
+			background-color: var(--tab-background-color) !important;
 			background-image: none !important;
 			-moz-window-dragging: no-drag !important;
 		  }
 		  :root[lwtheme] #addonbar {
-   			background-color: #1D1B22 !important;
+   			background-color: var(--tab-background-color) !important;
         	background-image: none !important;
 		  }
 		  :root[lwtheme][lwtheme-image='true'] #addonbar {
 			background-image: none !important;
-            background-color: #1D1B22 !important;
+            background-color: var(--tab-background-color) !important;
 			background-position: 0vw 50vh !important;
 		  }
 		  /* autohide add-on bar in fullscreen mode */
@@ -128,14 +128,14 @@ var AddAddonbar = {
 			display: block !important;
 			height: 1px !important;
 			max-height: 1px !important;
-            background-color: #1D1B22 !important;
+            background-color: var(--tab-background-color) !important;
         	background-image: none !important;
           }
 		  #main-window[sizemode='fullscreen']:not([inDOMFullscreen='true']) #addonbar:hover {
 			min-height: 26px !important;
 			height: 26px !important;
 			max-height: 26px !important;
-            background-color: #1D1B22 !important;
+            background-color: var(--tab-background-color) !important;
         	background-image: none !important;
 		  }
 		  #unified-extensions-button[hidden]{
